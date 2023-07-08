d6f80145ea10702894e4a8d454fdf7f381da6570
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-08 19:21:21 +0200

    Implement PayFastClient.php

--- src/Client/PayFastClient.php
+++ src/Client/PayFastClient.php
@@ -2,7 +2,38 @@
 
 namespace rainwaves\PayfastPayment\Client;
 
-class PayFastClient
-{
+use rainwaves\PayfastPayment\Contract\PayFastInterface;
+use rainwaves\PayfastPayment\Entities\SignatureTrait;
+use rainwaves\PayfastPayment\Form\FormBuilder;
+use rainwaves\PayfastPayment\Model\Sequence;
+use rainwaves\PayfastPayment\Request\PayFastRequest;
+use rainwaves\PayfastPayment\Validation\PayFastValidation;
 
+class PayFastClient implements PayFastInterface
+{
+    use SignatureTrait;
+    private \stdClass $config;
+    public function __construct(\stdClass $config)
+    {
+        $this->config = $config;
+    }
+
+    private PayFastRequest $request;
+    public function createForm(): string
+    {
+        $input = $this->request->toArray();
+        $signature = $this->generateSignature($input, 'jt7NOE43FZPn');
+        $input = Sequence::order($input);
+        $input['signature'] = $signature;
+        return FormBuilder::buildForm($input, $this->config->url);
+    }
+
+    public function makePaymentWithAForm(array $input): self
+    {
+        PayFastValidation::validate($input);
+        $input = (object)$input;
+        $this->request = new PayFastRequest($input, $this->config);
+
+        return $this;
+    }
 }
