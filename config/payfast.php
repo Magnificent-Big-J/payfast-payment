d23da20ff8338b0dff43a1cb61389c05ad4ae89b
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-08 19:19:08 +0200

    fixing the config file

--- config/payfast.php
+++ config/payfast.php
@@ -5,7 +5,7 @@ return [
   'merchant_id'=> env('MERCHANT_ID', '10000100'),
   'merchant_key'=> env('MERCHANT_KEY', '46f0cd694581a'),
   'env'=> env('ENVIRONMENT', 'local'),
-  'return_url'=> env('RETURN_URL', 'https://my.application.com/return'),
-  'cancel_url'=>env('CANCEL_URL', 'https://my.application.com/cancel'),
-  'notify_url'=>''
+  'return_url'=> env('RETURN_URL', 'https://www.example.com/success'),
+  'cancel_url'=> env('CANCEL_URL', 'https://www.example.com/cancel'),
+  'notify_url'=> env('NOTIFY_URL', 'https://www.example.com/notify'),
 ];
