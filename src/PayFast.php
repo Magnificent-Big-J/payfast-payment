2ac0eb029556ba92ecc096a735cd8ba186a34c4c
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-08 19:24:05 +0200

    Implement PayFast.php

--- src/PayFast.php
+++ src/PayFast.php
@@ -2,22 +2,38 @@
 
 namespace rainwaves\PayfastPayment;
 
+use rainwaves\PayfastPayment\Client\PayFastClient;
+use rainwaves\PayfastPayment\Contract\PayFastInterface;
 use stdClass;
 
-class PayFast
+class PayFast implements PayFastInterface
 {
     private const LOCAL = 'local';
     private const PRODUCTION = 'production';
     protected stdClass $config;
 
     private static array $sites = [
-      self::LOCAL => 'https://www.payfast.co.za/eng/process',
-      self::PRODUCTION => 'https://sandbox.payfast.co.za​/eng/process',
+      self::LOCAL => 'https://sandbox.payfast.co.za/eng/process' ,
+      self::PRODUCTION => 'https://www.payfast.co.za/eng/process',
     ];
 
+    private PayFastClient $payFastClient;
     public function __construct(array $config)
     {
         $config['url'] = self::$sites[$config['env']];
         $this->config = (object)$config;
+        $this->payFastClient = new PayFastClient($this->config);
+    }
+
+    public function createForm(): string
+    {
+        return  $this->payFastClient->createForm();
+    }
+
+    public function makePaymentWithAForm(array $input): PayFastClient
+    {
+        $this->payFastClient->makePaymentWithAForm($input);
+
+        return  $this->payFastClient;
     }
 }
