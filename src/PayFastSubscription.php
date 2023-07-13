3ae916bfda2517f7fdf1b92cf9b3e114d790169d
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-13 22:13:50 +0200

    Implement PayFastSubscription

--- src/PayFastSubscription.php
+++ src/PayFastSubscription.php
@@ -1,8 +1,29 @@
 <?php
 
-namespace rainwaves\PayfastPayment\Form;
+namespace rainwaves\PayfastPayment;
 
-class PayFastSubscription
+use rainwaves\PayfastPayment\Client\PayFastSubscriptionClient;
+use rainwaves\PayfastPayment\Contract\PayFastSubscriptionInterface;
+use rainwaves\PayfastPayment\Model\Route;
+use stdClass;
+
+class PayFastSubscription implements PayFastSubscriptionInterface
 {
+    protected stdClass $config;
+    private PayFastSubscriptionInterface $payFastSubscription;
+    public function __construct(array $config)
+    {
+        $config['url'] = Route::getUrl($config['env']);
+        $this->config = (object)$config;
+        $this->payFastSubscription = new PayFastSubscriptionClient($this->config);
+    }
+    public function createForm(): string
+    {
+        return $this->payFastSubscription->createForm();
+    }
 
+    public function createSubscriptionWithAForm(array $input): PayFastSubscriptionClient
+    {
+        return $this->payFastSubscription->createSubscriptionWithAForm($input);
+    }
 }
