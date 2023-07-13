a8ef56e0617040a7e61b8da07e941a63ff2c27d4
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-13 22:14:18 +0200

    Access url from Route

--- src/PayFast.php
+++ src/PayFast.php
@@ -4,23 +4,16 @@ namespace rainwaves\PayfastPayment;
 
 use rainwaves\PayfastPayment\Client\PayFastClient;
 use rainwaves\PayfastPayment\Contract\PayFastInterface;
+use rainwaves\PayfastPayment\Model\Route;
 use stdClass;
 
 class PayFast implements PayFastInterface
 {
-    private const LOCAL = 'local';
-    private const PRODUCTION = 'production';
     protected stdClass $config;
-
-    private static array $sites = [
-      self::LOCAL => 'https://sandbox.payfast.co.za/eng/process' ,
-      self::PRODUCTION => 'https://www.payfast.co.za/eng/process',
-    ];
-
     private PayFastClient $payFastClient;
     public function __construct(array $config)
     {
-        $config['url'] = self::$sites[$config['env']];
+        $config['url'] = Route::getUrl($config['env']);
         $this->config = (object)$config;
         $this->payFastClient = new PayFastClient($this->config);
     }
