483952311ba408c003651ae6f0c8560d550eb4a9
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-13 22:08:00 +0200

    Implement PayFastSubscriptionRequest

--- src/Request/PayFastSubscriptionRequest.php
+++ src/Request/PayFastSubscriptionRequest.php
@@ -2,7 +2,25 @@
 
 namespace rainwaves\PayfastPayment\Request;
 
-class PayFastSubscriptionRequest
+class PayFastSubscriptionRequest extends PayFastRequest
 {
+    public int $subscriptionType = 1;
+    public string $billingDate;
+    public float $recurringAmount;
+     public int $frequency;
+    public int $cycles = 0;
+    public bool $subscriptionNotifyEmail;
+    public bool $subscriptionNotifyWebhook;
+    public bool $subscriptionNotifyBuyer;
+    public function __construct(\stdClass $input, \stdClass $config)
+    {
+        parent::__construct($input, $config);
+        $this->billingDate = $input->billing_date;
+        $this->recurringAmount = $input->recurring_amount;
+        $this->frequency = $input->frequency;
+        $this->subscriptionNotifyEmail = $input->subscription_notify_email ?? false;
+        $this->subscriptionNotifyWebhook = $input->subscription_notify_webhook ?? false;
+        $this->subscriptionNotifyBuyer = $input->subscription_notify_buyer ?? false;
+    }
 
 }
